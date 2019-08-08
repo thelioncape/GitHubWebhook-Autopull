2b2c4a8d4d36300ddd49a188a8d18e0f48eb0623
thelioncape <thelioncape@gmail.com>
2019-08-08 23:22:25 +0100

    Create program Add location.txt Add .gitignore

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+// X52nqgk3wZG9A3oXa55mqLM71jJQPeRhMuVDC8Ua
+
+func main() {
+	wh := func(w http.ResponseWriter, _ *http.Request) {
+
+		f, err := os.Open("location.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s := bufio.NewScanner(f)
+		s.Scan()
+		location := s.Text()
+
+		cmd := exec.Command("cd", location)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		cmd = exec.Command("git pull")
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	http.HandleFunc("/X52nqgk3wZG9A3oXa55mqLM71jJQPeRhMuVDC8Ua", wh)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
